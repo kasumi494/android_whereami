9d736acb9251a0091789ebe571ef15fed01e62c6
kasumi <borovitina.ekaterina@gmail.com>
2013-11-19 22:48:15 +0600

    display curent location in text mode

--- WhereAmI/src/main/java/ru/x01d/whereami/MainActivity.java
+++ WhereAmI/src/main/java/ru/x01d/whereami/MainActivity.java
@@ -1,5 +1,8 @@
 package ru.x01d.whereami;
 
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationManager;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
@@ -10,6 +13,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.os.Build;
+import android.widget.TextView;
 
 public class MainActivity extends ActionBarActivity {
 
@@ -18,9 +22,19 @@ public class MainActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        LocationManager locationManager;
+        String context = Context.LOCATION_SERVICE;
+        locationManager = (LocationManager)getSystemService(context);
+
+        String provider = LocationManager.GPS_PROVIDER;
+        Location location =
+                locationManager.getLastKnownLocation(provider);
+
+        PlaceholderFragment placeholderFragment = new PlaceholderFragment(location);
+
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
+                    .add(R.id.container, placeholderFragment)
                     .commit();
         }
     }
@@ -49,16 +63,38 @@ public class MainActivity extends ActionBarActivity {
     /**
      * A placeholder fragment containing a simple view.
      */
-    public static class PlaceholderFragment extends Fragment {
+    public class PlaceholderFragment extends Fragment {
 
-        public PlaceholderFragment() {
+        View rootView;
+        TextView myLocationText;
+        Location location;
+
+        public PlaceholderFragment(Location location) {
+            this.location = location;
         }
 
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+            rootView = inflater.inflate(R.layout.fragment_main, container, false);
+            myLocationText = (TextView) rootView.findViewById(R.id.myLocationText);
+
+            updateWithNewLocation(location);
             return rootView;
+        }
+
+        private void updateWithNewLocation(Location location) {
+            String latLongString;
+
+            if (location != null)
+            {
+                double lat = location.getLatitude();
+                double lng = location.getLongitude();
+                latLongString = "Lat:" + lat + "\nLong:" + lng;
+            } else latLongString = "No location found";
+
+            myLocationText.setText("Your Current Position is:\n" +
+                    latLongString);
         }
     }
 
