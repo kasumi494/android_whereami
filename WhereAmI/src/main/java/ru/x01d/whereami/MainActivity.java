d35bc422c4fe532ab285cf8ed6ef7d8309ef61fc
kasumi <borovitina.ekaterina@gmail.com>
2013-11-19 23:54:07 +0600

    add criteria and updates

--- WhereAmI/src/main/java/ru/x01d/whereami/MainActivity.java
+++ WhereAmI/src/main/java/ru/x01d/whereami/MainActivity.java
@@ -1,7 +1,9 @@
 package ru.x01d.whereami;
 
 import android.content.Context;
+import android.location.Criteria;
 import android.location.Location;
+import android.location.LocationListener;
 import android.location.LocationManager;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBar;
@@ -14,23 +16,46 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.os.Build;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends ActionBarActivity implements LocationListener {
+    private LocationManager locationManager;
+    private String provider;
+    PlaceholderFragment placeholderFragment;
+
+    private static final int ONE_MINUTE = 1000 * 60;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        LocationManager locationManager;
         String context = Context.LOCATION_SERVICE;
         locationManager = (LocationManager)getSystemService(context);
 
-        String provider = LocationManager.GPS_PROVIDER;
+        Criteria criteria = new Criteria();
+        criteria.setAccuracy(Criteria.ACCURACY_FINE);
+        criteria.setCostAllowed(false);
+
+        provider = locationManager.getBestProvider(criteria, false);
+
+        boolean enabled = locationManager.isProviderEnabled(provider);
+
+        if (!enabled)
+        {
+            provider = locationManager.getBestProvider(criteria, true);
+            if (provider == null)
+            {
+                provider = locationManager.getBestProvider(criteria, false);
+                Toast.makeText(this, "Enable location service, please", Toast.LENGTH_LONG).show();
+            }
+        }
+
         Location location =
                 locationManager.getLastKnownLocation(provider);
 
-        PlaceholderFragment placeholderFragment = new PlaceholderFragment(location);
+        placeholderFragment = new PlaceholderFragment();
+        placeholderFragment.setLocation(location);
 
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
@@ -38,7 +63,6 @@ public class MainActivity extends ActionBarActivity {
                     .commit();
         }
     }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -60,6 +84,38 @@ public class MainActivity extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        locationManager.requestLocationUpdates(provider, ONE_MINUTE, 1, this);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        locationManager.removeUpdates(this);
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        placeholderFragment.updateWithNewLocation(location);
+    }
+
+    @Override
+    public void onStatusChanged(String s, int i, Bundle bundle) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String s) {
+        Toast.makeText(this, "Enabled provider: " + s, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onProviderDisabled(String s) {
+        Toast.makeText(this, "Disabled provider: " + s, Toast.LENGTH_SHORT).show();
+    }
+
     /**
      * A placeholder fragment containing a simple view.
      */
@@ -69,8 +125,7 @@ public class MainActivity extends ActionBarActivity {
         TextView myLocationText;
         Location location;
 
-        public PlaceholderFragment(Location location) {
-            this.location = location;
+        public PlaceholderFragment() {
         }
 
         @Override
@@ -95,6 +150,10 @@ public class MainActivity extends ActionBarActivity {
 
             myLocationText.setText("Your Current Position is:\n" +
                     latLongString);
+        }
+
+        public void setLocation(Location location) {
+            this.location = location;
         }
     }
 
